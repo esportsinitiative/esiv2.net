de48da1743b82ccc731ba9cefcd7a82a3b9cbc1b
David Soller <soller.23@osu.edu>
2015-03-21 03:39:48 -0400

    Created .NET Project

---
+++ esiv2.net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="esiv2.net.MvcApplication" Language="C#" %>
---
+++ esiv2.net/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(esiv2.net.Startup))]
+namespace esiv2.net
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
