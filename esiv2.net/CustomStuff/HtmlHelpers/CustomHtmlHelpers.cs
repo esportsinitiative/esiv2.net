740e4a0f77b2e69e37742e73bcf62bac45e49d20
David Soller <soller.23@osu.edu>
2015-03-22 03:21:55 -0400

    Added Games Page & Updated Image Html Helper

--- esiv2.net/CustomStuff/HtmlHelpers/CustomHtmlHelpers.cs
+++ esiv2.net/CustomStuff/HtmlHelpers/CustomHtmlHelpers.cs
@@ -11,15 +11,15 @@ namespace esiv2.net.CustomStuff.HtmlHelpers
         public static IHtmlString Image(this HtmlHelper helper, string src, string alt)
         {
             TagBuilder tb = new TagBuilder("img");
-            tb.Attributes.Add("src", VirtualPathUtility.ToAbsolute(src));
-            tb.Attributes.Add("alt", alt);
+            tb.Attributes.Add("src", VirtualPathUtility.ToAbsolute("~/Content/Images/" + src));
+            tb.Attributes.Add("alt", alt); // TODO - Fix picture Scaling
             return new MvcHtmlString(tb.ToString(TagRenderMode.SelfClosing));
         }
 
         public static IHtmlString Image(this HtmlHelper helper, string src, string alt, string attribute, string atrValue )
         {
             TagBuilder tb = new TagBuilder("img");
-            tb.Attributes.Add("src", VirtualPathUtility.ToAbsolute(src));
+            tb.Attributes.Add("src", VirtualPathUtility.ToAbsolute("~/Content/Images/" + src));
             tb.Attributes.Add("alt", alt);
             tb.Attributes.Add(attribute, atrValue);
             return new MvcHtmlString(tb.ToString(TagRenderMode.SelfClosing));
