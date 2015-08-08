fe829d7e2c713fc3dcb1e24ac17ec953f4a22f12
David Soller <soller.23@osu.edu>
2015-08-08 17:15:19 -0400

    Updated to Bootstrap 3

--- esiv2.net/App_Start/BundleConfig.cs
+++ esiv2.net/App_Start/BundleConfig.cs
@@ -20,12 +20,18 @@ namespace esiv2.net
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                        "~/Content/bootstrap.css",
+                        "~/Content/bootstrap-theme.css",
+                        "~/Content/site.css",
+                        "~/Content/bootstrap-responsive.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/oldIEBrowsersSupport").Include(
+                        "~/Scripts/respond.js",
+                        "~/Scripts/html5shiv.js"));
         }
     }
 }
